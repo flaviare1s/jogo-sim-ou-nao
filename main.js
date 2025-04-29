d05027a41b8baa42cd6c7e7f829abfab34aa054e
Flávia Reis <flaviarster@gmail.com>
2025-04-29 11:01:56 -0300

    feat: adiciona touch start para mobile

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ const btSim = document.getElementById('btnSim')
 const btNao = document.getElementById('btnNao')
 const resultado = document.getElementById('resultado')
 
-btNao.addEventListener('mousemove', () => {
+function moverBotao() {
   let x = Math.random() * (window.innerWidth - btNao.clientWidth)
   let y = Math.random() * (window.innerHeight - btNao.clientHeight)
 
   btNao.style.position = 'absolute'
   btNao.style.left = `${x}px`
   btNao.style.top = `${y}px`
-})
+}
+
+btNao.addEventListener('mousemove', moverBotao)
+btNao.addEventListener('touchstart', moverBotao)
+
 
 btSim.addEventListener('click', () => {
   resultado.classList.add('active')
