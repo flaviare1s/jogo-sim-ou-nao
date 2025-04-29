49122b8a7487348289e79c17c1914748e4a58022
flaviare1s <flaviarster@gmail.com>
2025-04-29 13:03:25 -0300

    fix: retorna a versão anterior

--- main.js
+++ main.js
@@ -12,20 +12,8 @@ function moverBotao() {
 }
 
 btNao.addEventListener('mousemove', moverBotao)
+btNao.addEventListener('touchmove', moverBotao)
 
-document.addEventListener("touchmove", (e) => {
-  const touch = e.touches[0]
-  const rect = btNao.getBoundingClientRect()
-
-  if (
-    touch.clientX >= rect.left &&
-    touch.clientX <= rect.right &&
-    touch.clientY >= rect.top &&
-    touch.clientY <= rect.bottom
-  ) {
-    moverBotao()
-  }
-})
 
 btSim.addEventListener('click', () => {
   resultado.classList.add('active')
